b609e566a46f9a4de09508c5f8165f4a12bb7366
daothiendang <daothiendang3@gmail.com>
2015-12-30 16:53:57 +0700

    Update page Category

--- wp-content/themes/isis/category.php
+++ wp-content/themes/isis/category.php
@@ -8,19 +8,13 @@
  * @since isis 1.5
  */
 
-
 get_header(); ?>
 <div class="row  warp">
-<div class="large-12 ">
- 
-<div style="min-height:500px; margin-top:40px;">
-
-<?php get_template_part(''.$isis = of_get_option('layout1_images').''); ?>
-
-
-
-</div>
-<?php get_template_part('pagination'); ?>  
-</div></div>
- 
-<?php get_footer(); ?>
+    <div class="large-12 ">
+        <div style="min-height:500px; margin-top:40px;">
+            <?php get_template_part('' . $isis = of_get_option('layout1_images') . ''); ?>
+        </div>
+        <?php get_template_part('pagination'); ?>  
+    </div>
+</div> 
+<?php get_footer();
