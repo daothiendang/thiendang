3df9bcb23fdc8073d7ccb5a1560d2a968befab02
daothiendang <daothiendang3@gmail.com>
2016-01-08 15:19:55 +0700

    add category "Tiếng Nhật" to sidebar

--- wp-content/themes/isis/sidebar.php
+++ wp-content/themes/isis/sidebar.php
@@ -1,6 +1,24 @@
 <div id="sidebar">
     <div class="widgets">          
-            <?php if ( is_active_sidebar('dynamic_sidebar') || !dynamic_sidebar('Right Sidebar') ) : ?> 
-            <?php endif; ?>
+        <?php if (is_active_sidebar('dynamic_sidebar') || !dynamic_sidebar('Right Sidebar')) {} ?>
+        <div id="archives-2" class="widget widget_archive">
+            <div class="widget_wrap">
+                <h3 class="widgettitle">Tiếng Nhật</h3>
+                <?php
+                    $siteUrl = get_site_url() . '/';
+                    $args = array('numberposts' => 5, 'category_name' => 'tieng-nhat');
+                    $posts = get_posts($args);
+                ?>
+                <ul>
+                    <?php foreach ($posts as $post) { ?>
+                        <li>
+                            <a href="<?php echo $siteUrl . $post->post_name; ?>">
+                                <?php echo $post->post_title; ?>
+                            </a>
+                        </li>
+                    <?php } ?>
+                </ul>
             </div>
+        </div>
     </div>
+</div>
