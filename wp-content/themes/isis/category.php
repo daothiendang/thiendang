3976a4c4844c28da5b9c2f04ce06cf2a528038e3
daothiendang <daothiendang3@gmail.com>
2016-08-29 17:13:17 +0700

    add comment section for category page

--- wp-content/themes/isis/category.php
+++ wp-content/themes/isis/category.php
@@ -12,8 +12,19 @@ get_header(); ?>
 <div class="row warp">
     <div id="content">
         <div class="top-content">
-            <?php get_template_part('' . $isis = of_get_option('layout1_images') . ''); ?>
-            <?php get_template_part('pagination'); ?>
+            <?php 
+                get_template_part('' . $isis = of_get_option('layout1_images') . '');
+                get_template_part('pagination');
+                $curCat = get_category ($cat);
+                $currentLink = $linkFbComment . 'category/' . $curCat->slug;
+                include(locate_template('share_this.php'));
+            ?>
+            <div style="margin:30px auto; width:90%">
+                <div class="fb-comments" data-href="<?php echo $currentLink; ?>" data-numposts="7" data-colorscheme="light"></div>
+            </div>
+            <div class="comments_template">
+                <?php comments_template('', true); ?>
+            </div>
         </div>
         <?php
             if (of_get_option('nosidebar_checkbox') == "0") {
