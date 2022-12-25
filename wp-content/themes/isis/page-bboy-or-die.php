df2efdd5164bd5aa17d692c48b6da36dbb7eee03
daothiendang <daothiendang3@gmail.com>
2022-12-25 16:12:37 +0700

    sửa tab => space

--- wp-content/themes/isis/page-bboy-or-die.php
+++ wp-content/themes/isis/page-bboy-or-die.php
@@ -15,50 +15,51 @@ get_header(); ?>
                     </div>
                     <div id="amazingslider-wrapper-1" style="display:block;position:relative;max-width:1000px;margin:0px auto 98px;">
                         <div id="amazingslider-1" style="display:block;position:relative;margin:0 auto;">
-							<?php
-							$bboy = array(
-								array("fujisan_airchair", "Airchair or die"),
-								array('skiing', 'Trượt tuyết Nagoya'),
-								array("paris_eiffel", "Paris - In the city of love"),
-								array("one_hand_stand", "Treo chuối một tay ở Big Ben :))"),
-								array("sing_marina_bay", "Hello from the horizontal side"),
-								array("malaysia_petronas_towers", "Tháp đôi Kuala Lumpur"),
-								array("amsterdam_windmill", "Breakdance windmill vs real Holland windmill :))"),
-								array("pisa_leaning_tower", "Leaning at Pisa - The leanning Tower"),
-								array("rome_colosseum", "Nike kick - Đấu trường La Mã"),
-								array("elbow_handstand", "Elbow freeze at Stonehenge"),
-								array("tokyo_tower_handhop", "Handhop - Tokyo đất chật người đông"),
-								array("osaka_castle", "Thành cổ osaka"),
-								array("kyoto_bamboo_forest", "Các chàng trai vàng trong làng airchair =))"),
-								array("airchair", "Airchair on a chair"),
-								array("airbaby_with_bending_leg", "airbaby gập chân"),
-								array("bntt", "Bước nhảy tuổi teen"),
-								array("bntt_2", "Bước nhảy tuổi teen"),
-								array("handstand", "Handstand"),
-								#array("handhop", "Nhảy chuối một tay"),
-								#array("handstand_1", ""),
-								array("headspin", "Quay đầu là hiphop"),
-								array("leaning_handstand", "Biển Quảng Bình"),
-								array("NiI", "NiI Crew"),
-								array("sidechair", "Sidechair"),
-								array("turtle_freeze", "Ke rùa một tay"),
-								array("vũ điệu trẻ", "Giải nhất Vũ điệu trẻ"),
-							);
-							
-							?>
+                            <?php
+                            $bboy = array(
+                                array("fujisan_airchair", "Airchair or die"),
+                                array('skiing', 'Trượt tuyết Nagoya'),
+                                array("paris_eiffel", "Paris - In the city of love"),
+                                array("one_hand_stand", "Treo chuối một tay ở Big Ben :))"),
+                                array("sing_marina_bay", "Hello from the horizontal side"),
+                                array("malaysia_petronas_towers", "Tháp đôi Kuala Lumpur"),
+                                array("amsterdam_windmill", "Breakdance windmill vs real Holland windmill :))"),
+                                array("pisa_leaning_tower", "Leaning at Pisa - The leanning Tower"),
+                                array("rome_colosseum", "Nike kick - Đấu trường La Mã"),
+                                array("elbow_handstand", "Elbow freeze at Stonehenge"),
+                                array("tokyo_tower_handhop", "Handhop - Tokyo đất chật người đông"),
+                                array("osaka_castle", "Thành cổ osaka"),
+                                array("odaiba_statue_of_liberty", "Nữ thần tự do phiên bản JAV full HD ko che"),
+                                array("kyoto_bamboo_forest", "Các chàng trai vàng trong làng airchair =))"),
+                                array("airchair", "Airchair on a chair"),
+                                array("airbaby_with_bending_leg", "airbaby gập chân"),
+                                array("bntt", "Bước nhảy tuổi teen"),
+                                array("bntt_2", "Bước nhảy tuổi teen"),
+                                array("handstand", "Handstand"),
+                                #array("handhop", "Nhảy chuối một tay"),
+                                #array("handstand_1", ""),
+                                array("headspin", "Quay đầu là hiphop"),
+                                array("leaning_handstand", "Biển Quảng Bình"),
+                                array("NiI", "NiI Crew"),
+                                array("sidechair", "Sidechair"),
+                                array("turtle_freeze", "Ke rùa một tay"),
+                                array("vũ điệu trẻ", "Giải nhất Vũ điệu trẻ"),
+                            );
+                            
+                            ?>
                             <ul class="amazingslider-slides" style="display:none;">
-								<?php
-								foreach ($bboy as $big_photo) {
-									echo '<li><img src="' . $themeUrl . '/images/bboy_or_die/' . $big_photo[0] . '.jpg" alt="' . $big_photo[0] . '"data-description="' . $big_photo[1] . '" /></li>';
-								}
-								?>
+                                <?php
+                                foreach ($bboy as $big_photo) {
+                                    echo '<li><img src="' . $themeUrl . '/images/bboy_or_die/' . $big_photo[0] . '.jpg" alt="' . $big_photo[0] . '"data-description="' . $big_photo[1] . '" /></li>';
+                                }
+                                ?>
                             </ul>
                             <ul class="amazingslider-thumbnails" style="display:none;">
-								<?php
-								foreach ($bboy as $small_photo) {
-									echo '<li><img src="' . $themeUrl . '/images/bboy_or_die/' . $small_photo[0] . '-tn.jpg" alt="' . $small_photo[0] . '"/></li>';
-								}
-								?>
+                                <?php
+                                foreach ($bboy as $small_photo) {
+                                    echo '<li><img src="' . $themeUrl . '/images/bboy_or_die/' . $small_photo[0] . '-tn.jpg" alt="' . $small_photo[0] . '"/></li>';
+                                }
+                                ?>
                             </ul>
                             <div class="amazingslider-engine"><a href="http://amazingslider.com" title="Responsive jQuery Image Slider">Responsive jQuery Image Slider</a></div>
                         </div>
